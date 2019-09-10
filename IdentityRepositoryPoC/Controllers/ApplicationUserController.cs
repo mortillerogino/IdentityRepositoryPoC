5ce722d691da4866c81d6e0ef0fb0fcc9e930d7d
Gino Mortillero <mortillerogino@gmail.com>
2019-09-10 15:53:23 +0800

    Added Login Api with JWT token authentication

--- IdentityRepositoryPoC/Controllers/ApplicationUserController.cs
+++ IdentityRepositoryPoC/Controllers/ApplicationUserController.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using IdentityRepositoryPoC.Data.Models;
 using IdentityRepositoryPoC.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityRepositoryPoC.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ApplicationUserController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOptions<AppSettings> _appSettings;
 
-        public ApplicationUserController(UserManager<ApplicationUser> userManager)
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
+            _appSettings = appSettings;
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [Route("Register")]
         //POST : /api/ApplicationUser/Register
-        public async Task<IActionResult> PostApplicationUser(ApplicationUserDto dto)
+        public async Task<IActionResult> RegisterUser(ApplicationUserDto dto)
         {
             var applicationUser = new ApplicationUser()
             {
@@ -41,6 +51,43 @@ namespace IdentityRepositoryPoC.Controllers
             {
                 return BadRequest(ex.ToString());
                 
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> LoginUser(ApplicationUserDto dto)
+        {
+            var user = await _userManager.FindByNameAsync(dto.UserName);
+            if (user != null)
+            {
+                if (await _userManager.CheckPasswordAsync(user, dto.Password))
+                {
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[]
+                        {
+                            new Claim("UserID", user.Id.ToString())
+                        }),
+                        Expires = DateTime.UtcNow.AddMinutes(30),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.Value.Secret)), SecurityAlgorithms.HmacSha256Signature)
+                    };
+
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                    var token = tokenHandler.WriteToken(securityToken);
+                    return Ok(new { token });
+                }
+                else
+                {
+                    return BadRequest("Password is incorrect.");
+                }
+            }
+            else
+            {
+                return BadRequest("Username does not exit.");
             }
         }
     }
--- IdentityRepositoryPoC/Startup.cs
+++ IdentityRepositoryPoC/Startup.cs
@@ -1,6 +1,8 @@
 using IdentityRepositoryPoC.Data.Data;
 using IdentityRepositoryPoC.Data.Data.EntityFramework;
 using IdentityRepositoryPoC.Data.Models;
+using IdentityRepositoryPoC.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,9 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace IdentityRepositoryPoC
 {
@@ -25,6 +30,9 @@ namespace IdentityRepositoryPoC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var appSettingSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingSection);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the Angular files will be served from this directory
@@ -52,6 +60,28 @@ namespace IdentityRepositoryPoC
                 options.Password.RequireUppercase = false;
                 options.Password.RequiredLength = 4;
             });
+
+            var appSettings = appSettingSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = true;
+                    x.SaveToken = false;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
